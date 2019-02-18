5b5c5625e4518a2e5dd61f27cf465900d5f3a3d3
Duncan Sterken <duncan.sterken123@gmail.com>
2019-02-18 15:43:18 +0100

    Add fade

--- LivingColors2.ino
+++ LivingColors2.ino
@@ -8,10 +8,20 @@
 
 #include <Arduino.h>
 
-#define SERIAL_RATE 115200;
+// IO
+#define diPresetRgb 7
+#define diPresetCmy 8
+#define aoRed 9
+#define aoGreen 10
+#define aoBlue 11
+#define aiPodMeter A0
+#define doLedTest 6
+
+#define SERIAL_RATE 115200
 
 // Globals
 bool enableDebug = true;
+#define DEMO_DELAY 20
 
 void printLn(String a_inputString) {
     if (enableDebug) {
@@ -25,13 +35,69 @@ void initSerial() {
 }
 
 void showCreator() {
-    printLn("Created by: Duncan \"duncte123\" Sterken")
+    printLn("Created by: Duncan \"duncte123\" Sterken");
+}
+
+int getLedFromNumber(int a_colorNumber) {
+
+    switch (a_colorNumber) {
+        case 0:
+            return aoRed;
+
+        case 1:
+            return aoGreen;
+
+        case 2:
+            return aoBlue;
+
+        default:
+            return aoRed;
+    }
+
+}
+
+void initDemo() {
+    printLn("INIT: Demo");
+
+    int colourNumber = 0;
+    int colourValue = 0;
+
+    // while loop to handle 3 colours
+    while (colourNumber < 3) {
+
+        int currentLed = getLedFromNumber(colourNumber);
+
+        printLn("Updating color to: " + (String) currentLed);
+
+        // loop to handle one color incrementing
+        while (colourValue < 256) {
+            analogWrite(currentLed, colourValue);
+
+            delay(DEMO_DELAY);
+
+            colourValue++;
+        }
+
+        // loop to handle one color decrementing
+        while (colourValue >= 0) {
+            analogWrite(currentLed, colourValue);
+
+            delay(DEMO_DELAY);
+
+            colourValue--;
+        }
+
+        colourNumber++;
+    }
+
+    printLn("READY: Demo");
 }
 
 void setup() {
 
     initSerial();
     showCreator();
+    initDemo();
 
 }
 
